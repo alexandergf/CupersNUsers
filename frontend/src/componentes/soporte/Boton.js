611e4cf32b0392d3cfee0d7985b17fcafaff97d8
pabloBP97 <pablo.bosch.perez.97@gmail.com>
2020-05-16 17:39:40 +0200

    Actualizacion

---
+++ frontend/src/componentes/soporte/Boton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { BsFillQuestionSquareFill } from 'react-icons/bs';
+
+export default class Boton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Boton;
