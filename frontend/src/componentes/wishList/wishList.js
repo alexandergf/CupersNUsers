f435fd29af87a36d711f0cda435c09f0da376d99
Alex <alexgferreiro96@gmail.com>
2020-05-19 15:48:46 +0200

    Boton de borrar todos los items de la lista de deseos acabado

--- frontend/src/componentes/wishList/wishList.js
+++ frontend/src/componentes/wishList/wishList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Card, Button } from 'react-bootstrap';
+import { Container, Card, Button, Modal } from 'react-bootstrap';
 import WishItems from './wishItems';
 import { instance } from '../../database/config';
 import axios from 'axios';
@@ -9,7 +9,10 @@ export default class wishList extends Component {
         super(props);
         this.state = {
             productos: [],
-            zeroProductos: false
+            zeroProductos: false,
+            show: false,
+            failRemove: false,
+            remove: false
         }
     }
 
@@ -51,15 +54,79 @@ export default class wishList extends Component {
           });
     }
 
+    deleteAllWishItems = () => {
+        this.setState({show: true})
+    }
+
+    totalRemove = () => {
+        let resultRemove = this.resultRemove;
+        axios.post('/wishlist/deleteAll', {}, instance)
+          .then(function (response) {
+            resultRemove(true);
+          })
+          .catch(function (error) {
+            resultRemove(false);
+          });
+    }
+
+    resultRemove = (value) => {
+        if(value){
+            this.setState({remove: true, show: false, productos: [], zeroProductos: true});
+        }else{
+            this.setState({failRemove: true, show: false});
+        }
+    }
+
     render() {
+        const modalRemove = <Modal show={this.state.show} onHide={() => this.setState({show: false})}>
+            <Modal.Header closeButton>
+            <Modal.Title>Confirmación</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Seguro que quieres borrar todos los articulos de la lista de deseos?</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => this.setState({show: false})}>
+                    Cancelar
+                </Button>
+                <Button variant="primary" onClick={() => this.totalRemove()}>
+                    Si, estoy de acuerdo
+                </Button>
+            </Modal.Footer>
+        </Modal>;
+
+        const errorRemove = <Modal show={this.state.failRemove} onHide={() => this.setState({failRemove: false})}>
+            <Modal.Header closeButton>
+            <Modal.Title>Ups</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Algo no ha ido bien.</Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={() => this.setState({failRemove: false})}>
+                    Vale
+                </Button>
+            </Modal.Footer>
+        </Modal>;
+
+        const acceptRemove = <Modal show={this.state.remove} onHide={() => this.setState({remove: false})}>
+            <Modal.Header closeButton>
+            <Modal.Title>Bien</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>La lista se ha vaciado con exito.</Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={() => this.setState({remove: false})}>
+                    Vale
+                </Button>
+            </Modal.Footer>
+        </Modal>;
         return (
             <Container fluid className="wish-perfil">
                 <Card>
-                    <Card.Title><h3 className="wish-title">Lista de deseos <Container><Button className="btn-delete-all">Borrar todos los productos</Button><Button className="btn-add-all">Añadir todo al carrito</Button></Container></h3></Card.Title>
+                    <Card.Title><h3 className="wish-title">Lista de deseos <Container><Button className="btn-delete-all" onClick={() => this.deleteAllWishItems()}>Borrar todos los productos</Button><Button className="btn-add-all">Añadir todo al carrito</Button></Container></h3></Card.Title>
                     <Card.Body>
                         {this.state.zeroProductos? "No hay productos en la lista de deseados.":<WishItems delete={this.deleteItemWishList.bind(this)} red={this.redireccionar.bind(this)} productos={this.state.productos} />}
                     </Card.Body>
                 </Card>
+                {modalRemove}
+                {errorRemove}
+                {acceptRemove}
             </Container>
         )
     }
